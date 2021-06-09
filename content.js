a01a29709cb3f5380b8c4bf4994a001724419e2a
Aamir <aamirsuhail01@yahoo.com>
2021-06-09 22:44:00 +0530

    optimized code for input elements

--- content.js
+++ content.js
@@ -1,7 +1,7 @@
 chrome.runtime.onMessage.addListener(
   function (request, sender, sendResponse) {
     if (request.message === "start") {
-     
+
       start();
     }
   }
@@ -9,50 +9,72 @@ chrome.runtime.onMessage.addListener(
 
 function start() {
   alert("started");
+
+  /* This will get the xpaths for elements with tag as input */
   var elements = document.getElementsByTagName("input");
   var len = elements.length;
   console.log(len);
-  
-  for(i=0; i<elements.length; i++){
-     var nam;
-      console.log(elements[i].hasAttribute("id"));
-      console.log(elements[i].hasAttribute("type"));
-      console.log(elements[i].getAttribute("type"));
-       if(elements[i].hasAttribute("id") && elements[i].getAttribute("type")!="hidden" && elements[i].getAttribute("style")==null){
-          nam = elements[i].getAttribute("id");
-           var xpat = "//input[@id=\""+nam+"\"]";
-           console.log(xpat);
-            elements[i].insertAdjacentHTML("afterend",xpat);
-            
+
+  for (i = 0; i < elements.length; i++) {
+    var nam;
+
+    if (elements[i].getAttribute("type") != "hidden") {
+
+      if (elements[i].getAttribute("style") == null) {
+        if (elements[i].hasAttribute("id")) {
+          input_getById();
+        }
+        else if (elements[i].hasAttribute("name")) {
+          input_getByname();
+
+        }
+        else if (elements[i].hasAttribute("placeholder")) {
+          input_getByplaceholder();
+
+        }
       }
-      else if(elements[i].hasAttribute("name") && elements[i].getAttribute("type")!="hidden" && elements[i].getAttribute("style")==null){
-          nam = elements[i].getAttribute("name");
-           var xpat = "//input[@name=\""+nam+"\"]";
-           console.log(xpat);
-            elements[i].insertAdjacentHTML("afterend",xpat);
-            
-      }
-      else if (elements[i].getAttribute("style")!=null){
-          if(!elements[i].getAttribute("style").includes("display")){
-                if(elements[i].hasAttribute("id")){
-                       nam = elements[i].getAttribute("id");
-                       var xpat = "//input[@id=\""+nam+"\"]";
-                       console.log(xpat);
-                       elements[i].insertAdjacentHTML("afterend",xpat);
-                }
-              else if(elements[i].hasAttribute("id")){
-                     nam = elements[i].getAttribute("name");
-                       var xpat = "//input[@name=\""+nam+"\"]";
-                       console.log(xpat);
-                        elements[i].insertAdjacentHTML("afterend",xpat);
-                }
-              }
+      else if (elements[i].getAttribute("style") != null) {
+        if (!elements[i].getAttribute("style").includes("display")) {
+
+          if (elements[i].hasAttribute("id")) {
+            input_getById();
           }
+
+          else if (elements[i].hasAttribute("name")) {
+            input_getByname();
+          }
+          else if (elements[i].hasAttribute("placeholder")) {
+            input_getByplaceholder();
+
+          }
+        }
       }
-  
-  
-      
-      
+    }
+  }
+
+
+
   alert("ended");
+}
+
+function input_getById() {
+  id = elements[i].getAttribute("id");
+  var xpat = "//input[@id=\"" + id + "\"]";
+  console.log(xpat);
+  elements[i].insertAdjacentHTML("afterend", xpat);
+}
+
+function input_getByname() {
+  nam = elements[i].getAttribute("name");
+  var xpat = "//input[@name=\"" + nam + "\"]";
+  console.log(xpat);
+  elements[i].insertAdjacentHTML("afterend", xpat);
+}
+
+function input_getByplaceholder() {
+  ph = elements[i].getAttribute("placeholder");
+  var xpat = "//input[@placeholder=\"" + ph + "\"]";
+  console.log(xpat);
+  elements[i].insertAdjacentHTML("afterend", xpat);
 }
 
