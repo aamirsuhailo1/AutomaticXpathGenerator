4680a3ead9cdaaf2bfeac8e7e72b7c099bfd5a6d
Aamir <aamirsuhail01@yahoo.com>
2021-06-13 12:21:28 +0530

    added message to display  after clicking on Generate button

--- popup.js
+++ popup.js
@@ -4,5 +4,6 @@ btn.addEventListener("click", function(){
     chrome.tabs.query({active: true, currentWindow: true}, function(tabs) {
     var activeTab = tabs[0];
     chrome.tabs.sendMessage(activeTab.id, {"message": "start"});
+    document.write("<h2 style='color:green';>Great !! Now copy the xpaths from the page and enjoy.</h4>");
   });
 });
