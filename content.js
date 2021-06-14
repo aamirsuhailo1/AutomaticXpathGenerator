0367e27f2699687acbf719fb3ecc324abe716282
Aamir <aamirsuhail01@yahoo.com>
2021-06-14 19:33:03 +0530

    optimized code

--- content.js
+++ content.js
@@ -1,26 +1,27 @@
 chrome.runtime.onMessage.addListener(
-  function (request, sender, sendResponse) {
-    if (request.message === "start") {
+    function (request, sender, sendResponse) {
+        if (request.message === "start") {
 
-      start();
+            start();
+        }
     }
-  }
 );
 
 function start() {
-  
-    var allElements = ["input","select","textarea","button","a"];
+
+    var allElements = ["input", "select", "textarea", "button", "a","div"];
 
     for (j = 0; j < allElements.length; j++) {
         var tagnam = allElements[j];
         var elements = document.getElementsByTagName(tagnam);
         var len = elements.length;
         console.log(len);
+        
         if (tagnam == "input") {
             console.log("inside input");
             for (i = 0; i < len; i++) {
                 var nam;
-    
+
                 if (elements[i].getAttribute("type") != "hidden") {
                     if (elements[i].getAttribute("style") == null) {
                         fetchXpaths(tagnam, elements[i]);
@@ -31,125 +32,126 @@ function start() {
                     }
                 }
             }
-    
-    
+
+
         }
-        else if (tagnam == "select") {
-            console.log("inside select");
+        else if (tagnam == "select" || tagnam == "textarea" || tagnam == "button" || tagnam == "a") {
             for (k = 0; k < len; k++) {
-                fetchXpaths(tagnam, elements[k]);
-            }
-        }
-        
-  
-           else if (tagnam == "textarea") {
-            console.log("inside textarea");
-            for (k = 0; k < len; k++) {
-                fetchXpaths(tagnam, elements[k]);
-            }
-        }
-        else if (tagnam == "button") {
-            console.log("inside button");
-            for (l = 0; l < len; l++) {
-                fetchXpaths(tagnam, elements[l]);
+                if (elements[k].getAttribute("style") == null) {
+                    fetchXpaths(tagnam, elements[k]);
+                } else if (elements[k].getAttribute("style") != null) {
+                    if (!elements[k].getAttribute("style").includes("display")) {
+                        fetchXpaths(tagnam, elements[k]);
+                    }
+                }
             }
         }
 
-        else if (tagnam == "a") {
-            console.log("inside link");
-            for (l = 0; l < len; l++) {
-                fetchXpaths(tagnam, elements[l]);
+        else if(tagnam == "div"){
+            for (k = 0; k < len; k++) {
+                if (elements[k].getAttribute("style") == null && elements[k].getAttribute("draggable") == "true") {
+                    fetchXpaths(tagnam, elements[k]);
+                } else if (elements[k].getAttribute("style") != null) {
+                    if (!elements[k].getAttribute("style").includes("display") && elements[k].getAttribute("draggable") == "true") {
+                        fetchXpaths(tagnam, elements[k]);
+                    }
+                }
             }
         }
+
     }
 }
-    
-    function fetchXpaths(tagnam, element) {
-        if (element.hasAttribute("id")) {
-            element_getById(tagnam, element);
-        } else if (element.hasAttribute("name")) {
-            element_getByname(tagnam, element);
-    
-        } else if (element.hasAttribute("formcontrolname")) {
-            element_getByformcontrolname(tagnam, element);
-    
-        } else if (element.hasAttribute("placeholder")) {
-            element_getByplaceholder(tagnam, element);
-    
-        } else if (element.hasAttribute("value")) {
-            element_getByvalue(tagnam, element);
-    
-        } else if (element.hasAttribute("role")) {
-            element_getByrole(tagnam, element);
-    
-        } else if (element.hasAttribute("class") && tagnam!="button" && tagnam!="a") {
-            element_getByclass(tagnam, element);   
-        }
-        else if(tagnam=="button" || tagnam=="a") {
-            element_getBytext(tagnam,element);
-        }
+
+function fetchXpaths(tagnam, element) {
+    if (element.hasAttribute("id")) {
+        element_getById(tagnam, element);
+    } else if (element.hasAttribute("name")) {
+        element_getByname(tagnam, element);
+
+    } else if (element.hasAttribute("formcontrolname")) {
+        element_getByformcontrolname(tagnam, element);
+
+    } else if (element.hasAttribute("placeholder")) {
+        element_getByplaceholder(tagnam, element);
+
+    } else if (element.hasAttribute("value")) {
+        element_getByvalue(tagnam, element);
+
+    } else if (element.hasAttribute("role")) {
+        element_getByrole(tagnam, element);
+
+    } else if (element.hasAttribute("class") && tagnam != "button" && tagnam != "a") {
+        element_getByclass(tagnam, element);
     }
-  
-    function element_getById(elementtype, elemt) {
-        id = elemt.getAttribute("id");
-        var xpat = "//" + elementtype + "[@id=\"" + id + "\"]";
-        console.log(xpat);
-        var el = "<table><tr><td style='border:1px dotted';color:red;><span style='color:green'>" + xpat + "</span></td></tr></table>";
-        elemt.insertAdjacentHTML("afterend", el );
+    else if (tagnam == "button" || tagnam == "a" || tagnam == "input") {
+        element_getBytext(tagnam, element);
     }
-    
-    function element_getByname(elementtype, elemt) {
-        nam = elemt.getAttribute("name");
-        var xpat = "//" + elementtype + "[@name=\"" + nam + "\"]";
-        console.log(xpat);
-         var el = "<table><tr><td style='border:1px dotted';color:red;><span style='color:green'>" + xpat + "</span></td></tr</table>";
-        elemt.insertAdjacentHTML("afterend", el );
-    }
-    
-    function element_getByformcontrolname(elementtype, elemt) {
-        fnam = elemt.getAttribute("formcontrolname");
-        var xpat = "//" + elementtype + "[@formcontrolname=\"" + fnam + "\"]";
-        console.log(xpat);
-          var el = "<table><tr><td style='border:1px dotted';color:red;><span style='color:green'>" + xpat + "</span></td></tr></table>";
-        elemt.insertAdjacentHTML("afterend", el );
-    }
-    
-    function element_getByvalue(elementtype, elemt) {
-        val = elemt.getAttribute("value");
-        var xpat = "//" + elementtype + "[@value=\"" + val + "\"]";
-        console.log(xpat);
-          var el = "<table><tr><td style='border:1px dotted';color:red;><span style='color:green'>" + xpat + "</span></td></tr></table>";
-        elemt.insertAdjacentHTML("afterend", el );
-    }
-    
-    function element_getByrole(elementtype, elemt) {
-        rol = elemt.getAttribute("role");
-        var xpat = "//" + elementtype + "[@role=\"" + rol + "\"]";
-        console.log(xpat);
-         var el = "<table><tr><td style='border:1px dotted';color:red;><span style='color:green'>" + xpat + "</span></td></tr></table>";
-        elemt.insertAdjacentHTML("afterend", el );
-    }
-    
-    function element_getByplaceholder(elementtype, elemt) {
-        ph = elemt.getAttribute("placeholder");
-        var xpat = "//" + elementtype + "[@placeholder=\"" + ph + "\"]";
-        console.log(xpat);
-          var el = "<table><tr><td style='border:1px dotted';color:red;><span style='color:green'>" + xpat + "</span></td></tr></table>";
-        elemt.insertAdjacentHTML("afterend", el );
-    }
-    
-    function element_getByclass(elementtype, elemt) {
-        cls = elemt.getAttribute("class");
-        var xpat = "//" + elementtype + "[@class=\"" + cls + "\"]";
-        console.log(xpat);
-          var el = "<table><tr><td style='border:1px dotted';color:red;><span style='color:green'>" + xpat + "</span></td></tr></table>";
-        elemt.insertAdjacentHTML("afterend", el );
-    }
-  
-  function element_getBytext(elementtype, elemt) {
-        itxt = elemt.innerText;
+}
+
+function element_getById(elementtype, elemt) {
+    id = elemt.getAttribute("id");
+    var xpat = "//" + elementtype + "[@id=\"" + id + "\"]";
+    console.log(xpat);
+    var el = "<table><tr><td style='border:1px dotted';color:red;><span style='color:green'>" + xpat + "</span></td></tr></table>";
+    elemt.insertAdjacentHTML("afterend", el);
+}
+
+function element_getByname(elementtype, elemt) {
+    nam = elemt.getAttribute("name");
+    var xpat = "//" + elementtype + "[@name=\"" + nam + "\"]";
+    console.log(xpat);
+    var el = "<table><tr><td style='border:1px dotted';color:red;><span style='color:green'>" + xpat + "</span></td></tr</table>";
+    elemt.insertAdjacentHTML("afterend", el);
+}
+
+function element_getByformcontrolname(elementtype, elemt) {
+    fnam = elemt.getAttribute("formcontrolname");
+    var xpat = "//" + elementtype + "[@formcontrolname=\"" + fnam + "\"]";
+    console.log(xpat);
+    var el = "<table><tr><td style='border:1px dotted';color:red;><span style='color:green'>" + xpat + "</span></td></tr></table>";
+    elemt.insertAdjacentHTML("afterend", el);
+}
+
+function element_getByvalue(elementtype, elemt) {
+    val = elemt.getAttribute("value");
+    var xpat = "//" + elementtype + "[@value=\"" + val + "\"]";
+    console.log(xpat);
+    var el = "<table><tr><td style='border:1px dotted';color:red;><span style='color:green'>" + xpat + "</span></td></tr></table>";
+    elemt.insertAdjacentHTML("afterend", el);
+}
+
+function element_getByrole(elementtype, elemt) {
+    rol = elemt.getAttribute("role");
+    var xpat = "//" + elementtype + "[@role=\"" + rol + "\"]";
+    console.log(xpat);
+    var el = "<table><tr><td style='border:1px dotted';color:red;><span style='color:green'>" + xpat + "</span></td></tr></table>";
+    elemt.insertAdjacentHTML("afterend", el);
+}
+
+function element_getByplaceholder(elementtype, elemt) {
+    ph = elemt.getAttribute("placeholder");
+    var xpat = "//" + elementtype + "[@placeholder=\"" + ph + "\"]";
+    console.log(xpat);
+    var el = "<table><tr><td style='border:1px dotted';color:red;><span style='color:green'>" + xpat + "</span></td></tr></table>";
+    elemt.insertAdjacentHTML("afterend", el);
+}
+
+function element_getByclass(elementtype, elemt) {
+    cls = elemt.getAttribute("class");
+    var xpat = "//" + elementtype + "[@class=\"" + cls + "\"]";
+    console.log(xpat);
+    var el = "<table><tr><td style='border:1px dotted';color:red;><span style='color:green'>" + xpat + "</span></td></tr></table>";
+    elemt.insertAdjacentHTML("afterend", el);
+}
+
+
+
+function element_getBytext(elementtype, elemt) {
+    itxt = elemt.innerText;
+    if(itxt!=""){
         var xpat = "//" + elementtype + "[text()=\"" + itxt + "\"]";
         console.log(xpat);
-          var el = "<table><tr><td style='border:1px dotted';color:red;><span style='color:green'>" + xpat + "</span></td></tr></table>";
-        elemt.insertAdjacentHTML("afterend", el );
+        var el = "<table><tr><td style='border:1px dotted';color:red;><span style='color:green'>" + xpat + "</span></td></tr></table>";
+        elemt.insertAdjacentHTML("afterend", el);
     }
+}
--- popup.js
+++ popup.js
@@ -4,6 +4,6 @@ btn.addEventListener("click", function(){
     chrome.tabs.query({active: true, currentWindow: true}, function(tabs) {
     var activeTab = tabs[0];
     chrome.tabs.sendMessage(activeTab.id, {"message": "start"});
-    document.write("<h2 style='color:green';>Great !! Now copy the xpaths from the page and enjoy.</h4>");
+    document.write("<div style='text-align:center; margin-top:100px;'><h2 style='color:green';>Great !! Now copy the xpaths from the page and enjoy.</h4></div>");
   });
 });
